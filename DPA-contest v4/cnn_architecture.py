ba029d1d82f7ec28fc677c125114f2e36880b31d
SpikingNeuron <praveenneuron@gmail.com>
2021-05-18 08:52:29 +0200

    add all plots and trained models and delete some files from master branch

--- DPA-contest v4/cnn_architecture.py
+++ DPA-contest v4/cnn_architecture.py
@@ -1,9 +1,11 @@
 import os.path
+import pathlib
 import sys
 import time
+import sys
 import numpy as np
 import pickle
-
+from keras.models import load_model
 from keras.models import Model
 from keras.layers import Flatten, Dense, Input, Conv1D, AveragePooling1D, BatchNormalization
 from keras.optimizers import Adam
@@ -11,6 +13,13 @@ from keras.callbacks import ModelCheckpoint
 from keras.utils import to_categorical
 from sklearn import preprocessing
 from exploit_pred import *
+from matplotlib.pyplot import figure
+
+if len(sys.argv) != 2:
+    raise Exception("Please supply training set id to train on ...")
+training_set_id = sys.argv[1]
+if training_set_id not in ['all', '1234']:
+    training_set_id = f"{int(training_set_id):02d}"
 
 ### Scripts based on ASCAD github : https://github.com/ANSSI-FR/ASCAD
 
@@ -89,42 +98,62 @@ def train_model(X_profiling, Y_profiling, X_test, Y_test, model, save_file_name,
 
 # Our folders
 root = "./"
-DPAv4_data_folder = root+"DPAv4_dataset/"
 DPAv4_trained_models_folder = root+"DPAv4_trained_models/"
 history_folder = root+"training_history/"
 predictions_folder = root+"model_predictions/"
+pathlib.Path(DPAv4_trained_models_folder).mkdir(exist_ok=True, parents=True)
+pathlib.Path(history_folder).mkdir(exist_ok=True, parents=True)
+pathlib.Path(predictions_folder).mkdir(exist_ok=True, parents=True)
 
 # Choose the hyperparameter's values
 nb_epochs = 50
 batch_size = 50
 input_size = 4000
 learning_rate = 1e-3
-nb_traces_attacks = 30
-nb_attacks = 100
-real_key = np.load(DPAv4_data_folder + "key.npy")
-mask = np.load(DPAv4_data_folder + "mask.npy")
-att_offset = np.load(DPAv4_data_folder + "attack_offset_dpav4.npy")
 
 start = time.time()
 
-# Load the profiling traces
-(X_profiling, Y_profiling), (X_attack, Y_attack), (plt_profiling, plt_attack) = (np.load(DPAv4_data_folder + 'profiling_traces_dpav4.npy'), np.load(DPAv4_data_folder + 'profiling_labels_dpav4.npy')), (np.load(DPAv4_data_folder + 'attack_traces_dpav4.npy'), np.load(DPAv4_data_folder + 'attack_labels_dpav4.npy')), (np.load(DPAv4_data_folder + 'profiling_plaintext_dpav4.npy'), np.load(DPAv4_data_folder + 'attack_plaintext_dpav4.npy'))
+def fetch_data(_set_id):
+    if _set_id == 'all':
+        _X = []
+        _Y = []
+        for _i in range(16):
+            (X_profiling, Y_profiling) = fetch_data(_set_id=f"{_i:02d}")
+            _X.append(X_profiling)
+            _Y.append(Y_profiling)
+        return (np.concatenate(_X, axis=0), np.concatenate(_Y, axis=0))
+    elif _set_id == '1234':
+        _X = []
+        _Y = []
+        for _i in range(4):
+            (X_profiling, Y_profiling) = fetch_data(_set_id=f"{_i:02d}")
+            _X.append(X_profiling)
+            _Y.append(Y_profiling)
+        return (np.concatenate(_X, axis=0), np.concatenate(_Y, axis=0))
+    else:
+        _ = int(_set_id)
+        DPAv4_data_folder = root + f"DPAv4_dataset_{_set_id}/"
+        # Load the profiling traces
+        # (X_profiling, Y_profiling), (X_attack, Y_attack), (plt_profiling, plt_attack) = (np.load(DPAv4_data_folder + 'profiling_traces_dpav4.npy'), np.load(DPAv4_data_folder + 'profiling_labels_dpav4.npy')), (np.load(DPAv4_data_folder + 'attack_traces_dpav4.npy'), np.load(DPAv4_data_folder + 'attack_labels_dpav4.npy')), (np.load(DPAv4_data_folder + 'profiling_plaintext_dpav4.npy'), np.load(DPAv4_data_folder + 'attack_plaintext_dpav4.npy'))
+        (X_profiling, Y_profiling) = (np.load(DPAv4_data_folder + 'profiling_traces_dpav4.npy'), np.load(DPAv4_data_folder + 'profiling_labels_dpav4.npy'))
+        return (X_profiling, Y_profiling)
 
 # Shuffle data
+(X_profiling, Y_profiling) = fetch_data(_set_id=training_set_id)
 (X_profiling, Y_profiling) = shuffle_data(X_profiling, Y_profiling)
 
 X_profiling = X_profiling.astype('float32')
-X_attack = X_attack.astype('float32')
+# X_attack = X_attack.astype('float32')
 
 #Standardization + Normalization (between 0 and 1)
-scaler = preprocessing.StandardScaler()
-X_profiling = scaler.fit_transform(X_profiling)
-X_attack = scaler.transform(X_attack)
+scaler_1 = preprocessing.StandardScaler()
+X_profiling = scaler_1.fit_transform(X_profiling)
+# X_attack = scaler.transform(X_attack)
 
-scaler = preprocessing.MinMaxScaler(feature_range=(0,1))
-X_profiling = scaler.fit_transform(X_profiling)
-X_attack = scaler.transform(X_attack)
-X_attack = X_attack.reshape((X_attack.shape[0], X_attack.shape[1], 1))
+scaler_2 = preprocessing.MinMaxScaler(feature_range=(0,1))
+X_profiling = scaler_2.fit_transform(X_profiling)
+# X_attack = scaler.transform(X_attack)
+# X_attack = X_attack.reshape((X_attack.shape[0], X_attack.shape[1], 1))
 
 
 #################################################
@@ -134,10 +163,11 @@ X_attack = X_attack.reshape((X_attack.shape[0], X_attack.shape[1], 1))
 
 #################################################
 #################################################
-
-# Choose your model
+#
+# # Choose your model
 model = cnn_architecture(input_size=input_size, learning_rate=learning_rate)
-model_name="DPA-contest_v4"
+model_name=f"DPA-contest_v4_trained_on_set_{training_set_id}"
+# model = load_model(DPAv4_trained_models_folder + model_name)
 
 print('\n Model name = '+model_name)
 
@@ -145,7 +175,22 @@ print('\n Model name = '+model_name)
 print("\n############### Starting Training #################\n")
 
 # Record the metrics
-history = train_model(X_profiling[:4000], Y_profiling[:4000], X_profiling[4000:], Y_profiling[4000:], model, DPAv4_trained_models_folder + model_name, epochs=nb_epochs, batch_size=batch_size)
+if training_set_id == 'all':
+    train_examples = 4000 * 16
+elif training_set_id == '1234':
+    train_examples = 4000 * 4
+else:
+    train_examples = 4000
+history = train_model(
+    X_profiling[:train_examples],
+    Y_profiling[:train_examples],
+    X_profiling[train_examples:],
+    Y_profiling[train_examples:],
+    model,
+    DPAv4_trained_models_folder + model_name,
+    epochs=nb_epochs,
+    batch_size=batch_size
+)
 end=time.time()
 
 print('Execution Time = %d'%(end-start))
@@ -166,27 +211,76 @@ with open(history_folder + 'history_' + model_name, 'wb') as file_pi:
 #################################################
 
 
-print("\n############### Starting Predictions #################\n")
+plot.clf()
+# plot.rcParams['figure.figsize'] = (2000, 1000)
+figure(figsize=(12, 10), dpi=400)
+plot.ylim(-5, 125)
+plot.grid(True)
+plot.xlabel('Number of traces')
+plot.ylabel('Rank')
+# legend = plot.legend(loc='upper left', bbox_to_anchor=(1, 1), borderaxespad=1, ncol=1)
+title = plot.title(
+    f"Rank plots for 16 fixed key datasets for model {model_name}"
+)
 
-predictions = model.predict(X_attack)
+nb_traces_attacks = 30
+nb_attacks = 100
 
-print("\n############### Predictions Done #################\n")
+for set_id in [f"{_:02d}" for _ in range(16)]:
+    DPAv4_data_folder = root + f"DPAv4_dataset_{set_id}/"
+    real_key = np.load(DPAv4_data_folder + "key.npy")
+    mask = np.load(DPAv4_data_folder + "mask.npy")
+    att_offset = np.load(DPAv4_data_folder + "attack_offset_dpav4.npy")
+    (X_attack, Y_attack, plt_attack) = (
+        np.load(DPAv4_data_folder + 'attack_traces_dpav4.npy'), np.load(DPAv4_data_folder + 'attack_labels_dpav4.npy'),
+        np.load(DPAv4_data_folder + 'attack_plaintext_dpav4.npy')
+    )
+    X_attack = X_attack.astype('float32')
+    X_attack = scaler_1.transform(X_attack)
+    X_attack = scaler_2.transform(X_attack)
+    X_attack = X_attack.reshape((X_attack.shape[0], X_attack.shape[1], 1))
 
-np.save(predictions_folder + 'predictions_' + model_name +'.npy', predictions)
+    print("\n############### Starting Predictions #################\n")
 
-#################################################
-#################################################
+    predictions = model.predict(X_attack)
 
-####            Perform attacks            ######
+    print("\n############### Predictions Done #################\n")
 
-#################################################
-#################################################
+    np.save(predictions_folder + 'predictions_' + model_name + f'_attacked_on_set_{set_id}.npy', predictions)
 
-print("\n############### Starting Attack on Test Set #################\n")
+    #################################################
+    #################################################
 
-avg_rank = perform_attacks(nb_traces_attacks, predictions, nb_attacks, plt=plt_attack, key=real_key, mask=mask, offset=att_offset, byte=0, filename=model_name)
+    ####            Perform attacks            ######
 
-print("\n t_GE = ")
-print(np.where(avg_rank<=0))
+    #################################################
+    #################################################
 
-print("\n############### Attack on Test Set Done #################\n")
+    print("\n############### Starting Attack on Test Set #################\n")
+
+    avg_rank = perform_attacks(nb_traces_attacks, predictions, nb_attacks, plt=plt_attack, key=real_key, mask=mask, offset=att_offset, byte=0, filename=model_name, savefig=False)
+
+    print("\n t_GE = ")
+    print(np.where(avg_rank<=0))
+
+    if set_id == training_set_id:
+        plot_type = '-'
+    else:
+        plot_type = ':'
+        if training_set_id == '1234':
+            if int(set_id) < 4:
+                plot_type = '-'
+
+    line_plot, = plot.plot(avg_rank, plot_type, label=f'set_{set_id} (key {real_key[0]:03d})')
+
+    print("\n############### Attack on Test Set Done #################\n")
+
+# plot and save to disk
+legend = plot.legend(loc='upper left', bbox_to_anchor=(1, 1), borderaxespad=1, ncol=1)
+plot.savefig(
+    f"fig/rank_all_sets_with_{nb_attacks}_attacks_for_model_{model_name}.svg",
+    format='svg',
+    # dpi=1200,
+    bbox_extra_artists=(legend,),
+    bbox_inches='tight'
+)
---
+++ Modifications.md
@@ -0,0 +1,75 @@
+## Modifications
+
+This branch `fixed_key_bias` is intended to do experiment with only `DPA-contest v4`
+This is because it has multiple fixed key datasets.
+
+We have uploaded all 16 datasets with attack and profile traces.
+[Download link](https://drive.google.com/file/d/1Ol4fcvq2Nu0pRLn-yRu7vptQ-xTywgP7/view?usp=sharing).
+
+Note that training will happen on datasets we provide as it is not possible to replicate the steps from 
+original authors. We can verify things are still similar based on rank plots.
+
+## Installation instructions
+
+Note that the original code is old and will need a python 3.7 installation.
+Plus make sure that you have correct versions of some libraries (mentioned below)
+The code is not compatible with newer version so consider downgrading some libraries.
+
+```bat
+pip install tensorflow==1.13.1
+pip install keras==2.2.4
+pip install h5py==2.10.0
+pip install numpy==1.16.4
+pip install scipy==1.5.4
+```
+
+Other libraries that will be needed
+
+```bat
+pip install -U scikit-learn
+pip install matplotlib
+```
+
+## Train on one dataset and attack on all 16 datasets
+
+### Neural-Net was also trained on the `set_00`
+![](./DPA-contest%20v4/fig/rank_all_sets_with_100_attacks_for_model_DPA-contest_v4_trained_on_set_00.svg)
+### Neural-Net was also trained on the `set_01`
+![](./DPA-contest%20v4/fig/rank_all_sets_with_100_attacks_for_model_DPA-contest_v4_trained_on_set_01.svg)
+### Neural-Net was also trained on the `set_02`
+![](./DPA-contest%20v4/fig/rank_all_sets_with_100_attacks_for_model_DPA-contest_v4_trained_on_set_02.svg)
+### Neural-Net was also trained on the `set_03`
+![](./DPA-contest%20v4/fig/rank_all_sets_with_100_attacks_for_model_DPA-contest_v4_trained_on_set_03.svg)
+### Neural-Net was also trained on the `set_04`
+![](./DPA-contest%20v4/fig/rank_all_sets_with_100_attacks_for_model_DPA-contest_v4_trained_on_set_04.svg)
+### Neural-Net was also trained on the `set_05`
+![](./DPA-contest%20v4/fig/rank_all_sets_with_100_attacks_for_model_DPA-contest_v4_trained_on_set_05.svg)
+### Neural-Net was also trained on the `set_06`
+![](./DPA-contest%20v4/fig/rank_all_sets_with_100_attacks_for_model_DPA-contest_v4_trained_on_set_06.svg)
+### Neural-Net was also trained on the `set_07`
+![](./DPA-contest%20v4/fig/rank_all_sets_with_100_attacks_for_model_DPA-contest_v4_trained_on_set_07.svg)
+### Neural-Net was also trained on the `set_08`
+![](./DPA-contest%20v4/fig/rank_all_sets_with_100_attacks_for_model_DPA-contest_v4_trained_on_set_08.svg)
+### Neural-Net was also trained on the `set_09`
+![](./DPA-contest%20v4/fig/rank_all_sets_with_100_attacks_for_model_DPA-contest_v4_trained_on_set_09.svg)
+### Neural-Net was also trained on the `set_10`
+![](./DPA-contest%20v4/fig/rank_all_sets_with_100_attacks_for_model_DPA-contest_v4_trained_on_set_10.svg)
+### Neural-Net was also trained on the `set_11`
+![](./DPA-contest%20v4/fig/rank_all_sets_with_100_attacks_for_model_DPA-contest_v4_trained_on_set_11.svg)
+### Neural-Net was also trained on the `set_12`
+![](./DPA-contest%20v4/fig/rank_all_sets_with_100_attacks_for_model_DPA-contest_v4_trained_on_set_12.svg)
+### Neural-Net was also trained on the `set_13`
+![](./DPA-contest%20v4/fig/rank_all_sets_with_100_attacks_for_model_DPA-contest_v4_trained_on_set_13.svg)
+### Neural-Net was also trained on the `set_14`
+![](./DPA-contest%20v4/fig/rank_all_sets_with_100_attacks_for_model_DPA-contest_v4_trained_on_set_14.svg)
+### Neural-Net was also trained on the `set_15`
+![](./DPA-contest%20v4/fig/rank_all_sets_with_100_attacks_for_model_DPA-contest_v4_trained_on_set_15.svg)
+
+
+## Train on a mix of all 16 dataset's and attack on all 16 datasets
+
+![](./DPA-contest%20v4/fig/rank_all_sets_with_100_attacks_for_model_DPA-contest_v4_trained_on_set_all.svg)
+
+## Train on a mix of first four dataset's and attack on all 16 datasets
+
+![](./DPA-contest%20v4/fig/rank_all_sets_with_100_attacks_for_model_DPA-contest_v4_trained_on_set_1234.svg)
