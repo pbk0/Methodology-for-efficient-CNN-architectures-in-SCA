bc7d3bf1efd0d2a713b6e6ca9f57780285b2e9f3
SpikingNeuron <praveenneuron@gmail.com>
2021-06-04 22:33:49 +0200

    script to get all experiments stats with hist plot

--- ASCAD/N0=0/run_100_experiments_stats.py
+++ ASCAD/N0=0/run_100_experiments_stats.py
@@ -9,30 +9,41 @@ from matplotlib.ticker import PercentFormatter
 
 RESULTS_DIR = pathlib.Path("save_results")
 
-ranks_fig = go.Figure()
+ranks_fig_non_converged = go.Figure()
+ranks_fig_converged = go.Figure()
 _min_traces_per_experiment = []
-for _dir in RESULTS_DIR.iterdir():
-    _experiment_id = int(_dir.name)
+for _experiment_id in range(100):
+    _dir = RESULTS_DIR / str(_experiment_id)
+    # noinspection PyTypeChecker
     _ranks = np.load(_dir / "avg_rank_ASCAD_desync0.npy")
     _traces_with_rank_0 = np.where(_ranks <= 0)[0]
     if len(_traces_with_rank_0) > 0:
         _min_traces_per_experiment.append(
             _traces_with_rank_0[0]
         )
+        ranks_fig_converged.add_trace(
+            go.Scatter(
+                x=np.arange(len(_ranks)),
+                y=_ranks,
+                mode='lines',
+                name=f"exp_{_experiment_id:03d}"
+            )
+        )
     else:
         _min_traces_per_experiment.append(
             np.inf
         )
-    ranks_fig.add_trace(
-        go.Scatter(
-            x=np.arange(len(_ranks)),
-            y=_ranks,
-            mode='lines',
-            name=f"exp_{_experiment_id:03d}"
+        ranks_fig_non_converged.add_trace(
+            go.Scatter(
+                x=np.arange(len(_ranks)),
+                y=_ranks,
+                mode='lines',
+                name=f"exp_{_experiment_id:03d}"
+            )
         )
-    )
 
-ranks_fig.show()
+ranks_fig_non_converged.show()
+ranks_fig_converged.show()
 
 _experiments_that_did_not_converge = [i for i, v in enumerate(_min_traces_per_experiment) if v == np.inf]
 _min_traces_for_converged_experiments = [v for i, v in enumerate(_min_traces_per_experiment) if v != np.inf]
