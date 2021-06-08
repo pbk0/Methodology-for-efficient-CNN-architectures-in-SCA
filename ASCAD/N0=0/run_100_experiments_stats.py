168483cb8e7c4eddab50fdd78a3b46a89d9d90a0
SpikingNeuron <praveenneuron@gmail.com>
2021-06-08 08:54:16 +0200

    updating code for loss plots

--- ASCAD/N0=0/run_100_experiments_stats.py
+++ ASCAD/N0=0/run_100_experiments_stats.py
@@ -2,6 +2,7 @@ import pathlib
 import numpy as np
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
+import pickle
 from matplotlib import colors
 import plotly.express as px
 from matplotlib.ticker import PercentFormatter
@@ -9,13 +10,46 @@ from matplotlib.ticker import PercentFormatter
 
 RESULTS_DIR = pathlib.Path("save_results_original")
 
-ranks_fig_non_converged = go.Figure()
-ranks_fig_converged = go.Figure()
+ranks_fig_non_converged = go.Figure(
+    layout=go.Layout(
+        title=go.layout.Title(text="Average Rank (non-converged)")
+    )
+)
+ranks_fig_converged = go.Figure(
+    layout=go.Layout(
+        title=go.layout.Title(text="Average Rank (converged)")
+    )
+)
+train_loss_fig_non_converged = go.Figure(
+    layout=go.Layout(
+        title=go.layout.Title(text="Train Loss (non-converged)")
+    )
+)
+train_loss_fig_converged = go.Figure(
+    layout=go.Layout(
+        title=go.layout.Title(text="Train Loss (converged)")
+    )
+)
+val_loss_fig_non_converged = go.Figure(
+    layout=go.Layout(
+        title=go.layout.Title(text="Validation Loss (non-converged)")
+    )
+)
+val_loss_fig_converged = go.Figure(
+    layout=go.Layout(
+        title=go.layout.Title(text="Validation Loss (converged)")
+    )
+)
 _min_traces_per_experiment = []
 for _experiment_id in range(100):
     _dir = RESULTS_DIR / str(_experiment_id)
     # noinspection PyTypeChecker
     _ranks = np.load(_dir / "avg_rank_ASCAD_desync0.npy")
+    with open((_dir / 'history_ASCAD_desync0').as_posix(), 'rb') as file_pi:
+        print(_dir / 'history_ASCAD_desync0')
+        _history = pickle.load(file_pi)
+    _train_loss = _history['loss']
+    _val_loss = _history['val_loss']
     _traces_with_rank_0 = np.where(_ranks <= 0)[0]
     if len(_traces_with_rank_0) > 0:
         _min_traces_per_experiment.append(
@@ -25,6 +59,22 @@ for _experiment_id in range(100):
             go.Scatter(
                 x=np.arange(len(_ranks)),
                 y=_ranks,
+                mode='lines',
+                name=f"exp_{_experiment_id:03d}"
+            )
+        )
+        train_loss_fig_converged.add_trace(
+            go.Scatter(
+                x=np.arange(len(_train_loss)),
+                y=_train_loss,
+                mode='lines',
+                name=f"exp_{_experiment_id:03d}"
+            )
+        )
+        val_loss_fig_converged.add_trace(
+            go.Scatter(
+                x=np.arange(len(_val_loss)),
+                y=_val_loss,
                 mode='lines',
                 name=f"exp_{_experiment_id:03d}"
             )
@@ -41,9 +91,29 @@ for _experiment_id in range(100):
                 name=f"exp_{_experiment_id:03d}"
             )
         )
+        train_loss_fig_non_converged.add_trace(
+            go.Scatter(
+                x=np.arange(len(_train_loss)),
+                y=_train_loss,
+                mode='lines',
+                name=f"exp_{_experiment_id:03d}"
+            )
+        )
+        val_loss_fig_non_converged.add_trace(
+            go.Scatter(
+                x=np.arange(len(_val_loss)),
+                y=_val_loss,
+                mode='lines',
+                name=f"exp_{_experiment_id:03d}"
+            )
+        )
 
 ranks_fig_non_converged.show()
 ranks_fig_converged.show()
+train_loss_fig_non_converged.show()
+train_loss_fig_converged.show()
+val_loss_fig_non_converged.show()
+val_loss_fig_converged.show()
 
 _experiments_that_did_not_converge = [i for i, v in enumerate(_min_traces_per_experiment) if v == np.inf]
 _min_traces_for_converged_experiments = [v for i, v in enumerate(_min_traces_per_experiment) if v != np.inf]
